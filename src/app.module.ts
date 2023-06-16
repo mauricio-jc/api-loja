cf9155a014f694ba3e884df6472d5418178fd9da
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2023-06-16 13:40:06 -0300

    Criado modules, controllers e services.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CategoriesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
